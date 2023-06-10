550dfa3b3814b747d3459da60c3b4748d23aed58
Ramon <ramondiiaas@hotmail.com>
2023-06-10 17:56:22 -0300

    some fixes in eslint

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -6,7 +6,7 @@ export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
-    .map(day => formatter.format(new Date(Date.UTC(2021, 5, day))))
+    .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
     .map((weekDay) => {
       if(short) {
         return weekDay.substring(0, 3).toUpperCase()
